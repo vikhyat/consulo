5d777aeeddb240fae32be182da17637f32cd003e
Vikhyat Korrapati <vikhyatk@gmail.com>
2012-04-24 11:53:18 +0530

    moved requirements to readme

--- server/app.rb
+++ server/app.rb
@@ -13,19 +13,3 @@ config["network_elements"].each do |ip, oids|
 end
 
 pp poll_variables
-
-
-__END__
-Operations to be supported:
-- Poll a new OID.
-- List all tracked OIDs.
-- Delete an OID.
-- LDCs:
-  - Add an LDC
-  - Remove an LDC
-- Data query
-
-Details of tracked OIDs needs to be stored somewhere persistent.
-For now they will be stored in a file, but it would be wise to make this
-something more robust like a Redis store or something.
-
