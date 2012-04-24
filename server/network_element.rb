ae18a71b64514e323dfc07c16da93a3c8c0a3b82
Vikhyat Korrapati <vikhyatk@gmail.com>
2012-04-24 11:28:19 +0530

    added ping functionality to network element class

--- server/network_element.rb
+++ server/network_element.rb
@@ -1,6 +1,12 @@
+require 'ping'
 
 class NetworkElement
   def initialize(ip_address)
     @ip_address = ip_address
+  end
+
+  # ping the network element to see whether it is up.
+  def up?(timeout=3)
+    Ping.pingecho(@ip_address, timeout)
   end
 end
