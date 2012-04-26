ee3407319e198dd888c873adc861b2a8d934c481
Vikhyat Korrapati <vikhyatk@gmail.com>
2012-04-26 11:13:36 +0530

    moved reactor class into its own file

--- ldc/app.rb
+++ ldc/app.rb
@@ -1,33 +1,4 @@
-# coding: utf-8
-require 'zmq'
-require 'msgpack'
-
-class Reactor
-  def initialize(id)
-    $context ||= ZMQ::Context.new(1)
-    @responder = $context.socket(ZMQ::REP)
-    @responder.bind(id)
-  end
-
-  def deactivate
-    @responder.close
-  end
-
-  def handle_request(r)
-    if r == "PING"
-      return "PONG"
-    else
-      return "INVALID"
-    end
-  end
-
-  def run
-    loop do
-      request = MessagePack.unpack @responder.recv
-      @responder.send(handle_request(request).to_msgpack)
-    end
-  end
-end
+require_relative 'reactor.rb'
 
 reactor = Reactor.new("tcp://*:9861")
 
